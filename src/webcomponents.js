ad513e5e9436e0fcaef7f5a750750cf0328887f6
Dale Sande <dale.sande@gmail.com>
2020-12-11 12:56:26 -0800

    style: remove legacy Orion depednencies

--- src/webcomponents.js
+++ src/webcomponents.js
@@ -6,7 +6,6 @@ import "@alaskaairux/auro-datetime";
 
 // hyperlink
 import "@alaskaairux/auro-hyperlink";
-import "@alaskaairux/ods-hyperlink";
 
 // alerts
 import "@alaskaairux/auro-alerts";
@@ -15,7 +14,6 @@ import "@alaskaairux/auro-alerts";
 import "@alaskaairux/auro-avatar";
 
 // button
-import "@alaskaairux/ods-button";
 import "@alaskaairux/auro-button";
 
 // icon
@@ -42,9 +40,6 @@ import "@alaskaairux/auro-swatch/dist/auro-swatch-list";
 import "@alaskaairux/auro-swatch/dist/auro-tokens-list";
 import "@alaskaairux/auro-swatch/dist/auro-color-avatar";
 
-// legacy Orion input type text
-import "@alaskaairux/ods-inputtext";
-
 // input type text
 import "@alaskaairux/auro-input";
 
@@ -52,14 +47,9 @@ import "@alaskaairux/auro-input";
 import "@alaskaairux/auro-header";
 
 // input type radio/checkbox
-import "@alaskaairux/ods-inputoptions";
 import "@alaskaairux/auro-checkbox";
 import "@alaskaairux/auro-checkbox/dist/auro-checkbox-group";
 import "@alaskaairux/auro-radio";
 import "@alaskaairux/auro-radio/dist/auro-radio-group";
 
 import "@alaskaairux/auro-popover";
-
-
-// toast
-import "@alaskaairux/ods-toast";
